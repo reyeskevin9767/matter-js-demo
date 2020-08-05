958ed64d4c8e6c9a15e0c27e3d3cdb00543ff22e
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-04 23:45:50 -0400

    added walls and mousecontraint

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 //* Runner - Coordinate the Engine and World
 //* World - Snapshot of the world
 //* Bodies - Geometry in the world
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
 const engine = Engine.create();
 const { world } = engine;
+
+//* Creates a canvas
 const render = Render.create({
   element: document.body,
   engine: engine,
@@ -19,8 +21,29 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
+//* Allows shapes to be grab
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
+
+//* Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+];
+
+//* Add Walls to the world
+World.add(world, walls);
+
+//* Create a new shape
 const shape = Bodies.rectangle(200, 200, 50, 50, {
   isStatic: true,
 });
 
+//* Add shape to the world
 World.add(world, shape);
